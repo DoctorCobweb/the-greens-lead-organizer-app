20dfa0e2796dfc8319deb19f54bcb535be125438
Doctor Cobweb <spinninghalf@gmail.com>
2014-04-08 15:08:55 +1000

    Initial Commit

---
+++ TheGreensLeadOrganizerApp/TGLOAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TGLOAppDelegate.h
+//  TheGreensLeadOrganizerApp
+//
+//  Created by andre on 8/04/2014.
+//  Copyright (c) 2014 andre trosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TGLOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TheGreensLeadOrganizerApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TheGreensLeadOrganizerApp
+//
+//  Created by andre on 8/04/2014.
+//  Copyright (c) 2014 andre trosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TGLOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TGLOAppDelegate class]));
+    }
+}
